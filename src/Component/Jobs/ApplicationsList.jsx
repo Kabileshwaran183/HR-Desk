c9a5c40b623848dad999c124d0e2d3cd78f19ce7
vgsanjaykumar <143058056+vgsanjaykumar@users.noreply.github.com>
2025-03-13 13:17:49 +0530

    final

--- src/Component/Jobs/ApplicationsList.jsx
+++ src/Component/Jobs/ApplicationsList.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import API_URL from "../../config";  
+
 
 const ApplicationsList = () => {
     const [applications, setApplications] = useState([]);
@@ -7,15 +9,18 @@ const ApplicationsList = () => {
         fetchApplications();
     }, []);
 
+
+
     const fetchApplications = async () => {
         try {
-            const response = await fetch("http://localhost:5000/api/applications");
+            const response = await fetch(`${API_URL}/api/applications`);
             const data = await response.json();
             setApplications(data);
         } catch (error) {
-            console.error("❌ Error fetching applications:", error);
+            console.error("Error fetching applications:", error);
         }
     };
+
 
     return (
         <div className="max-w-4xl mx-auto p-6 mt-10 bg-white shadow-lg rounded-lg">
@@ -33,7 +38,7 @@ const ApplicationsList = () => {
                             <th className="border p-2">Skills</th>
                             <th className="border p-2">Location</th>
                             <th className="border p-2">Pincode</th>
-                                <th className="border p-2">Resume </th>
+                            <th className="border p-2">Resume </th>
                         </tr>
                     </thead>
                     <tbody>
