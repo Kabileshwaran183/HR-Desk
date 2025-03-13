6100737db282217ff70d032f19a104b378cbfaf7
vgsanjaykumar <143058056+vgsanjaykumar@users.noreply.github.com>
2025-03-13 15:29:10 +0530

    render

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({ origin: "*" })); // Allow all origins (for testing)
+app.use(cors());
 app.use(express.json());
 
 // MongoDB Connection
